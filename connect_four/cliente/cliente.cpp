b65279fb5dc5023a92d11ffdfea37f4427d150d7
frenubb <francisco.barraza2001@alumnos.ubiobio.cl>
2024-06-01 17:01:58 -0400

    Makefiles cliente/servidor y cliente.cpp

---
+++ connect_four/cliente/cliente.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+#define PORT 7777
+
+void jugar(int sock);
+
+int main(int argc, char const *argv[]) {
+	if (argc != 3) {
+		std::cerr << "Uso: "<< argv[0] << "<IP_SERVIDOR> <PUERTO>" << std::endl;
+		return -1
+	}
+
+	const char *server_ip =argv[1];
+	int port = std::stoi(argv[2]);
+
+	int sock = 0;
+	struct sockaddr_in serv_addr;
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) <0){
+		std:cerr << "cliente.cpp main -> Error al crear el socket" << std::endl;
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port)
+
+	if (inet_pton(AF_INT, server_ip, $serv_addr.sin_addr) <=0){
+		std__cerr << "cliente.cpp main -> Direccion ip no valida" << std::endl;
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		std::cerr << "cliente.cpp main -> Error de conexion << std::endl;"
+		return -1;
+	}
+
+	jugar(sock);
+
+	close(sock);
+	return 0;
+}
+
+void jugar(int sock) {
+	char buffer[1024] = {0}
+	while (true){
+		int valread = read(sock, buffer, 1024)
+		if(valread<=0){
+			break;
+		}
+		std::cout << buffer << std::endl
+
+		std::string input;
+		std::cout << "Introduzca columna(0-6): ";
+		std::cin >> input;
+		send(sock, input.c_str(), input.length(), 0);
+	}
+}
